699a8777ef47e6d65430e7083acebff8f500d5c6
draganmarjanovic <gagalug13@gmail.com>
2015-06-24 23:38:04 +1000

    NaN Input Handling
    Text inputs now handle NaN inputs.

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var widmark_factor = {
 var main = this;
 
 main.user = { "sex" : "male",
-              "weight" : 81,
+              "weight" : 85.9,
               "bac" : 0.00,
               "drive" : false,
               "time" : 0
@@ -25,8 +25,8 @@ main.user = { "sex" : "male",
 
 
 main.drinks = { "Beer" : {"count" : 0, "alcohol_content" : 4.9, "std_volume" : .375, "img" : "beer.png"},
-                "Wine" : {"count" : 0, "alcohol_content" : 11.6, "std_volume" : .15, "img" : "beer.png"},
-                "Spirits" : {"count" : 0, "alcohol_content" : 45, "std_volume" : .03, "img" : "beer.png"},
+                "Wine" : {"count" : 0, "alcohol_content" : 11.6, "std_volume" : .15, "img" : "wine.png"},
+                "Spirits" : {"count" : 0, "alcohol_content" : 45, "std_volume" : .03, "img" : "spirits.png"},
                };
 
 
@@ -69,7 +69,7 @@ main.newDrink = function(){
     // drink_ID = main.drinks.length;
     drink_ID = Object.keys(main.drinks).length;
     console.log(drink_ID);
-    main.drinks[drink_ID] = {"count" : 0, "alcohol_content" : 45, "std_volume" : 0.03, "img" : "beer.png"};
+    main.drinks[drink_ID] = {"count" : 0, "alcohol_content" : 45, "std_volume" : 0.03, "img" : "defaults.png"};
 };
 
 main.setSex = function(sex){
@@ -78,20 +78,47 @@ main.setSex = function(sex){
 };       
 
 main.setAlcoholContent = function(drink_ID, content){
-    main.drinks[drink_ID]["alcohol_content"] = content;
+    if (content < 0 || isNaN(content)) {
+        main.drinks[drink_ID]["alcohol_content"] = main.drinks[drink_ID]["alcohol_content"];
+    }
+    else{
+        main.drinks[drink_ID]["alcohol_content"] = content;
+    }
     console.log(drink_ID);
     console.log(content);
     bac_update();
-}
+};
+
+main.setAlcoholVolume = function(drink_ID, volume){
+    volume = volume/1000;
+    if (volume < 0 || isNaN(volume)) {
+       main.drinks[drink_ID]["std_volume"] = main.drinks[drink_ID]["std_volume"]; 
+    }
+    else{
+       main.drinks[drink_ID]["std_volume"] = volume;
+    }
+    console.log(drink_ID);
+    console.log(volume);
+    bac_update();
+};
 
 main.setWeight = function (weight){
     // Sets the weight
-    main.user["weight"] = weight;
+    if (weight < 0 || isNaN(weight)) {
+        main.user["weight"] = main.user["weight"];
+    }
+    else{
+        main.user["weight"] = weight;
+    }
     bac_update();
 };
 
 main.setTime = function (elapsed_time) {
-    main.user["time"] = elapsed_time;
+    if (elapsed_time < 0 || isNaN(elapsed_time)) {
+        main.user["time"] = main.user["time"];
+    }else{
+        main.user["time"] = elapsed_time;
+    }
     bac_update();
 };
 
